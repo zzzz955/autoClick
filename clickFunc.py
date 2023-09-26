ae6cadb42569d406c79529c87dfa4a8e74d38dba
zzzz955 <zzzzz955@gmail.com>
2023-09-26 17:42:36 +0900

    오토 클릭 모듈 작성

--- clickFunc.py
+++ clickFunc.py
@@ -1,10 +1,23 @@
 import time
 import pyautogui
+from PyQt5.QtCore import Qt
 
 
-def click_start(interval):
-    x, y = pyautogui.position()
-    while 1:
-        pyautogui.click(x, y)
-        time.sleep(interval)
+class click_Func:
+    def __init__(self):
+        self.roof = None
+
+    def key_pressed(self, key):
+        if key == Qt.Key_F3:
+            self.roof = True
+            self.click_start(1)
+        if key == Qt.Key_F4:
+            self.roof = False
+
+    def click_start(self, interval):
+        x, y = pyautogui.position()
+
+        while self.roof:
+            pyautogui.click(x, y)
+            time.sleep(interval)
 
